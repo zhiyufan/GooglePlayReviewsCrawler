aef6f35576c2b5220e73ce47c3d3cf6af3a0f0d6
David <sustc1526fan@hotmail.com>
2019-03-04 11:20:10 +0800

    optimize

---
+++ AppList.py
@@ -0,0 +1,108 @@
+app_ip = [
+    #'org.kiwix.kiwixmobile',                                   ### NICE download crash
+    #'com.artifex.mupdfdemo' ###PDF CRASH
+    #'org.isoron.uhabits',
+    #'com.oristats.habitbull',
+    #'info.intrasoft.habitgoaltracker',
+    #'com.imaginstudio.imagetools.pixellab',
+    #'org.mozilla.focus',
+    #'com.android.gpstest'
+    #'com.medium.reader',
+    #'com.ideashower.readitlater.pro',
+    #'com.neatometer.android'
+    #'com.weebly.android'
+    #'notion.id'
+    #'com.aisense.otter'
+    #'com.SouthernPacificOceanFisher.VoiceToText_memo'
+    #'rish.crearo.lifehacks'                                     ### Feedback crash
+    #'com.lifehack.life_hack'
+    #'com.airvisual'
+    #'com.simplehabit.simplehabitapp',
+    #'com.calm.android',
+    #'org.stopbreathethink.app',
+    #'com.smilingmind.app'
+    #'app.story.craftystudio.shortstory'
+    #'com.tapdir.success.psychology'
+    #'com.tapdir.learnenglish'
+    #'com.getsomeheadspace.android',
+
+    #'com.spotlightsix.zentimerlite2'
+    #'org.wikipedia',
+    #'org.ligi.survivalmanual'
+    #'com.goodreads'
+    #'com.elasthink.lyricstraining'
+    #'com.timeanddate.countdown'                                     1.4.9 setting upgrade  share
+    #'org.pixelrush.moneyiq'
+    #'in.usefulapp.timelybills'
+
+    #'net.techet.netanalyzerlite.an'
+    #'com.arkadiusz.dayscounter'
+    #'com.sociosoft.countdown'
+    #'com.brunoschalch.timeuntil'
+    #'com.pione.questiondiary',
+    #'com.epiphany.lunadiary'
+    #'com.bookmark.money',
+    #'com.devhd.feedly'
+    #'cache.optimize.cleancache.clearcache.cachecleaner.clean.clear.boost.booster.junk'
+    #'com.astrovicapp.bigDaysLite'
+    #'com.brunoschalch.timeuntil'
+    #'org.M.alcodroid'
+    #'net.nebulium.wiki'
+    #'com.b_lam.resplash'
+    #'com.feresr.walpy'
+    #'partl.dailypic'
+    #'com.thambu.lockscreenwallpaper.free'
+    #'com.xnview.hypocam'
+
+    #'com.xnview.inmage'
+    #'com.hdwallpaper.wallpaper4k'
+    #'com.innologica.inoreader'
+    #'com.wered.sensorsmultitool'
+    #'com.knappily.media',
+    #'com.abhi.newmemo',
+    #'com.humanhelper.forhuman.quick',
+    #'com.nisahnt.nishantbafna.notifydemo',
+    #'com.teamlease.associate',
+
+    #'com.studionmd.whitenoise',
+    #'ice.lenor.nicewordplacer.app'
+    #'com.truatvl.englishgrammartest.dev',
+    #'com.djonique.birdays',
+    #'com.ToDoReminder.gen'
+    #'com.tasks.android'                                               1.10    local backup
+    #'com.kabouzeid.gramophone'
+    #'com.kongzue.tofuture',
+    #'com.nearify.android'
+    #'com.dripgrind.mindly'
+    #'com.starttoday.android.wear'
+
+    #'com.meisterlabs.meistertask.native'
+    #'com.formagrid.airtable'                                            1.2.2 download any media
+    #'x1Trackmaster.x1Trackmaster'
+    #'io.edo.agenda'
+    #'com.jellyworkz.mubert'
+    #'com.mysugr.android.companion',
+    #'com.read.app'
+    #'com.pexels.app'
+    #'com.nibbleapps.fitmencook'
+    #'com.workflowy.android',
+    #'com.dvdb.bergnotes',
+    #'net.kreosoft.android.mynotes'
+    #'com.designs1290.tingles'
+    #'com.slowlyapp'
+    #'com.mywickr.wickr2'
+    #'another.music.player'
+    #'orphic.eclectik.com.orphic'
+    #'app.Xeasec.writer'
+    #'it.returntrue.novelist'
+    #'com.diffathy.bbapp'
+    #'sanity.podcast.freak'
+
+
+    #"com.seatgeek.android", 111
+    #'com.wrike'             111
+    #'com.meisterlabs.meistertask.native'
+    #'com.asana.app'
+    #'com.wetransfer.app.live'
+    'com.radiolight.etatsunis'
+]
--- GooglePlayReviewCrawler.py
+++ GooglePlayReviewCrawler.py
@@ -2,121 +2,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import json
-
+from AppList import app_ip
 drivers = []
-#drivers.append(webdriver.Chrome())
-#drivers.append(webdriver.Chrome())
-#drivers.append(webdriver.Chrome())
 driver = webdriver.Chrome()
-#wait = WebDriverWait(driver, 10)
-
-app_ip = [
-    #'org.kiwix.kiwixmobile',                                   ### NICE download crash
-    #'com.artifex.mupdfdemo' ###PDF CRASH
-    #'org.isoron.uhabits',
-    #'com.oristats.habitbull',
-    #'info.intrasoft.habitgoaltracker',
-    #'com.imaginstudio.imagetools.pixellab',
-    #'org.mozilla.focus',
-    #'com.android.gpstest'
-    #'com.medium.reader',
-    #'com.ideashower.readitlater.pro',
-    #'com.neatometer.android'
-    #'com.weebly.android'
-    #'notion.id'
-    #'com.aisense.otter'
-    #'com.SouthernPacificOceanFisher.VoiceToText_memo'
-    #'rish.crearo.lifehacks'                                     ### Feedback crash
-    #'com.lifehack.life_hack'
-    #'com.airvisual'
-    #'com.simplehabit.simplehabitapp',
-    #'com.calm.android',
-    #'org.stopbreathethink.app',
-    #'com.smilingmind.app'
-    #'app.story.craftystudio.shortstory'
-    #'com.tapdir.success.psychology'
-    #'com.tapdir.learnenglish'
-    #'com.getsomeheadspace.android',
-
-    #'com.spotlightsix.zentimerlite2'
-    #'org.wikipedia',
-    #'org.ligi.survivalmanual'
-    #'com.goodreads'
-    #'com.elasthink.lyricstraining'
-    #'com.timeanddate.countdown'                                     1.4.9 setting upgrade  share
-    #'org.pixelrush.moneyiq'
-    #'in.usefulapp.timelybills'
-
-    #'net.techet.netanalyzerlite.an'
-    #'com.arkadiusz.dayscounter'
-    #'com.sociosoft.countdown'
-    #'com.brunoschalch.timeuntil'
-    #'com.pione.questiondiary',
-    #'com.epiphany.lunadiary'
-    #'com.bookmark.money',
-    #'com.devhd.feedly'
-    #'cache.optimize.cleancache.clearcache.cachecleaner.clean.clear.boost.booster.junk'
-    #'com.astrovicapp.bigDaysLite'
-    #'com.brunoschalch.timeuntil'
-    #'org.M.alcodroid'
-    #'net.nebulium.wiki'
-    #'com.b_lam.resplash'
-    #'com.feresr.walpy'
-    #'partl.dailypic'
-    #'com.thambu.lockscreenwallpaper.free'
-    #'com.xnview.hypocam'
-
-    #'com.xnview.inmage'
-    #'com.hdwallpaper.wallpaper4k'
-    #'com.innologica.inoreader'
-    #'com.wered.sensorsmultitool'
-    #'com.knappily.media',
-    #'com.abhi.newmemo',
-    #'com.humanhelper.forhuman.quick',
-    #'com.nisahnt.nishantbafna.notifydemo',
-    #'com.teamlease.associate',
-
-    #'com.studionmd.whitenoise',
-    #'ice.lenor.nicewordplacer.app'
-    #'com.truatvl.englishgrammartest.dev',
-    #'com.djonique.birdays',
-    #'com.ToDoReminder.gen'
-    #'com.tasks.android'                                               1.10    local backup
-    #'com.kabouzeid.gramophone'
-    #'com.kongzue.tofuture',
-    #'com.nearify.android'
-    #'com.dripgrind.mindly'
-    #'com.starttoday.android.wear'
-
-    #'com.meisterlabs.meistertask.native'
-    #'com.formagrid.airtable'                                            1.2.2 download any media
-    #'x1Trackmaster.x1Trackmaster'
-    #'io.edo.agenda'
-    #'com.jellyworkz.mubert'
-    #'com.mysugr.android.companion',
-    #'com.read.app'
-    #'com.pexels.app'
-    #'com.nibbleapps.fitmencook'
-    #'com.workflowy.android',
-    #'com.dvdb.bergnotes',
-    #'net.kreosoft.android.mynotes'
-    #'com.designs1290.tingles'
-    #'com.slowlyapp'
-    #'com.mywickr.wickr2'
-    #'another.music.player'
-    #'orphic.eclectik.com.orphic'
-    #'app.Xeasec.writer'
-    #'it.returntrue.novelist'
-    #'com.diffathy.bbapp'
-    #'sanity.podcast.freak'
-
-
-    #"com.seatgeek.android", 111
-    #'com.wrike'             111
-    #'com.meisterlabs.meistertask.native'
-    #'com.asana.app'
-    'com.wetransfer.app.live'
-]
 
 show_more_class_tag = "RveJvd"
 all_reviews_tag = "UD7Dzf"
@@ -141,7 +29,7 @@ def search_show_more():
     return len(driver.find_elements_by_class_name(show_more_class_tag))
 
 def is_crash_exist(review):
-    crash_words = ["crash", "freeze"]
+    crash_words = ["crash"]#, "freeze"]
     for word in crash_words:
         if word in review.lower():
             return True
@@ -196,7 +84,7 @@ while app_num < len(app_ip):
             final_json.append(result)
     if len(final_json) > 0:
         output = json.dumps(final_json)
-        fp = open(app_ip[app_num]+".json", "w")
+        fp = open("json_file/"+app_ip[app_num]+".json", "w")
         fp.write(output)
         fp.close()
     app_num += 1
